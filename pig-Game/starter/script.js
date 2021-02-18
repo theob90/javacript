f477c2f3daea4a062dfb14c2338759f6a8c7f81d
theob90 <duski.bez@gmail.com>
2021-02-18 20:58:18 +0200

    'dice'

--- pig-Game/starter/script.js
+++ pig-Game/starter/script.js
@@ -1,1 +1,94 @@
 'use strict';
+
+
+//selecting  Elements
+//# giati exw ID
+const score0El = document.querySelector('#score--0');
+// const score = getElementById('score--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+let currentScore;
+let player;
+let playing;
+let scores; 
+
+const init = function(){
+    scores = [0,0];
+    currentScore = 0;
+    player = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    //vazw kai hidden sto css
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('remove--winner');
+    player1El.classList.remove('remove--winner');
+    player0El.classList.add('remove--active');
+    player1El.classList.remove('remove--active');
+
+
+
+};
+
+init();
+
+const switchPlayer = function(){
+    document.getElementById(`current--${player}`).textContent = 0;
+    currentScore = 0;
+    player = player === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+};
+
+
+
+
+
+//roll dice
+btnRoll.addEventListener('click', function(){
+    if (playing){
+        
+    
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    if(dice !== 1){
+        currentScore = currentScore + dice;
+        document.getElementById(`score--${player}`).textContent = currentScore;
+        // current0El.textContent = currentScore;
+    }else{
+        switchPlayer();
+    }
+}
+});
+
+btnHold.addEventListener('click', function(){
+    if(playing){    
+        scores[player] += currentScore;
+        document.getElementById(`score--${player}`).textContent = scores[player];
+        if(scores[player]>=10){
+            playing = false;
+            document.querySelector(`.player--${player}`).classList.add('player--winner');
+            document.querySelector(`.player--${player}`).classList.remove('player--active');
+        }else{
+            switchPlayer();
+
+        }
+        
+}
+});
+
+btnNew.addEventListener('click',  init);
--- starter/script.js
+++ starter/script.js
@@ -33,23 +33,77 @@
 
 // console.log(fruitProccess(2,3));
 
-const  calcAge = function (birthYear){
-    return 2021 - birthYear;
-} 
+// const  calcAge = function (birthYear){
+//     return 2021 - birthYear;
+// } 
 
-const yearToRetirement = function (birthYear, firstName)  {
-    const age = calcAge(birthYear);
-    const retirement = 65 - age;
+// const yearToRetirement = function (birthYear, firstName)  {
+//     const age = calcAge(birthYear);
+//     const retirement = 65 - age;
 
-    if(retirement> 0){
-        return `${firstName} retires in ${retirement}  years`;
+//     if(retirement> 0){
+//         return `${firstName} retires in ${retirement}  years`;
 
-    }else{
-        return `already retired`;
-    }
+//     }else{
+//         return `${firstName} already retired`;
+//     }
 
-    // return retirement;
-}
+//     // return retirement;
+// }
 
-console.log(yearToRetirement(1990, `theo`));
-console.log(yearToRetirement(1950, `theo`));
+// console.log(yearToRetirement(1990, `theo`));
+// console.log(yearToRetirement(1950, `thddddddddeo`));
+
+// const friend1 = 'Theo';
+// const friend2 = 'deps';
+// const friend3 = 'paparia';
+
+// const friends = ['Theo', 'deps','paparia'];
+// console.log (friends);
+
+// const years = new Array (1993,2002,2200);
+// console.log(years);
+
+// console.log(friends[0]);
+
+// console.log(friends.length);
+
+// const calcAge = function(birthYear){
+//     return 2021 - birthYear;
+// }
+
+// const years1 = [1990,2200];
+
+// console.log(calcAge(years1));
+
+
+// const age1 = calcAge(years1[0]);
+// console.log(age1);
+
+
+
+// const friends = ['theo', 'deps']
+// friends.push('foufo');
+// console.log(friends);
+
+
+const jonas = {
+    first: 'theo',
+    last:'bez',
+    age:'33',
+    job:'soft'
+
+};
+
+console.log(jonas.age);
+
+// const interested = prompt('what do you want to know of jonas? choose first last age job');
+
+// console.log(interested);
+
+jonas.location ='Patra';
+jonas.friends ='3';
+jonas.bff='Michael';
+console.log(jonas);
+
+console.log('${jonas.first} has ${jonas.friends}`);
